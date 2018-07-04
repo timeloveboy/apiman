28bdfc66a3f98fb6ecc94f3866c13168a7144396
timeloveboy <734991033@qq.com>
2018-07-04 23:50:16 +0800

    ~

--- htmlpart/renderhtml.go
+++ htmlpart/renderhtml.go
@@ -1,27 +1,28 @@
 package htmlpart
 
 import (
-
 	"io/ioutil"
 	"regexp"
 	"strings"
 )
 
 const (
-	Regex_Loadtemplate = `<!--{{LoadTemplate(.{0,20})}}-->`
+	Regex_Loadtemplate = `<!--{{LoadTemplate(.{0,200})}}-->`
 )
 
 func Render(root, url, html string) string {
 	reg := regexp.MustCompile(Regex_Loadtemplate)
 	var mc []string
 	mc = reg.FindAllString(html, -1)
+
 	var result string = html
 	for i := 0; i < len(mc); i++ {
 		var tag string = mc[i]
 		var str = tag[19 : len(tag)-6]
 		var pagename = strings.TrimSpace(str)
 		nodepath := ""
-
+		print(tag)
+		print("\r\n")
 		if pagename[0] == '/' {
 
 			nodepath = root + "/"
